065feede6ede2b3335411ff84bb28ef52153d39c
rodriiiiiiiiiiiii <rodrimen@ucm.es>
2025-09-20 21:06:28 +0200

    Cambios en Main.java y Mano.java

--- src/poker/Main.java
+++ src/poker/Main.java
@@ -2,9 +2,8 @@ package poker;
 
 public class Main {
     public static void main(String[] args) {
-        Mano m = Mano.desdeString10("AhAdQhJhTh");
+        Mano m = Mano.desdeString10("AhKhQhJhTh");
         Mano.Resultado r = m.evaluar();
-
         System.out.println("Best hand: " + r.descripcion);
         for (String d : m.detectarDraws()) {
             System.out.println("Draw: " + d);
